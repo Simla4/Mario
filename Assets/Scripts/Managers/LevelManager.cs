20217b91923de5d2164c130e16064915613ca769
Simla Beken <38881186+Simla4@users.noreply.github.com>
2023-03-15 16:16:30 +0300

    feat: procedural level generation

---
+++ Assets/Scripts/Managers/LevelManager.cs
@@ -0,0 +1,77 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Tilemaps;
+using Random = UnityEngine.Random;
+
+public class LevelManager : MonoBehaviour
+{
+    #region Variables
+
+    [SerializeField] private int levelWidth;
+    [SerializeField] private int levelHeight;
+
+    [SerializeField] private Sprite ground;
+    [SerializeField] private Tilemap tilemap;
+    [SerializeField] private Tile tile;
+
+    private int[,] levelMap;
+
+    #endregion
+
+    #region MyRegion
+
+    private void Start()
+    {
+        GenerateArrayList();
+        RenderMap();
+    }
+
+    #endregion
+
+    #region OtherMethods
+
+    private void GenerateArrayList()
+    {
+        levelMap = new int[levelWidth, levelHeight];
+        
+        var minHeightVal = levelHeight - 2;
+
+        for (int i = 0; i < levelWidth; i++)
+        {
+            var height = Random.Range(minHeightVal, (levelHeight + 1));
+            
+            for (int j = 0; j < levelHeight; j++)
+            {
+                if (j < height)
+                {
+                    levelMap[i, j] = 1;
+                }
+                else
+                {
+                    levelMap[i, j] = 0;
+                }
+            }
+        }
+    }
+
+    private void RenderMap()
+    {
+        tilemap.ClearAllTiles();
+        
+        for (int x = 0; x < levelWidth; x++)
+        {
+            for (int y = 0; y < levelHeight; y++)
+            {
+                if (levelMap[x, y] == 1)
+                {
+                    tilemap.SetTile(new Vector3Int(x, y, 0), tile);
+                }
+                
+            }
+        }
+    }
+
+    #endregion
+}
